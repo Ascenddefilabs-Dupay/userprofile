221b190574f2b8426833cb474a60e87b6d12606e
anjalikalburgi09 <anjali.kalburgi@ascenddefi.com>
2024-10-30 19:36:16 +0530

    deleted unwanted code

--- backend-userprofile/profileupdate/models.py
+++ backend-userprofile/profileupdate/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Project(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-
-
-    def __str__(self):
-        return self.name
     
 class CustomUser(models.Model):
     user_id = models.CharField(max_length=8, primary_key=True)
--- backend-userprofile/profileupdate/serializers.py
+++ backend-userprofile/profileupdate/serializers.py
@@ -1,11 +1,5 @@
 from rest_framework import serializers
-from .models import Project,CustomUser
-
-
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
+from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
